248f791592e097f9810196c267eeaa4534f94221
Michael Small <michaelsmalldev@gmail.com>
2026-04-09 21:52:12 -0500

    chore: give overview of this demo project

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,25 @@ import { Links } from './links';
   imports: [Links],
   template: `
     <h1>Signal Forms + NgRx + NgRx Toolkit</h1>
+
     <app-links />
+
+    <p>
+      This demo project will probably be a weird mix of signal stores and not signal stores for the
+      moment because these are a bunch of prototypes slapped together and some speculation from me
+      (Michael Small) who is NOT on the NgRx team. Though I am a member of the NgRx Toolkit.
+    </p>
+    <p>
+      The Toolkit is currently working on extending withResource to support widening beyond taking a
+      ResourceRef (exposes reload) to also take a regular Resource (subset of the ref, doesn't
+      support reload). So currently, extendedResources cannot just be thrown into withResource from
+      the Toolkit.
+    </p>
+    <p>
+      Will extendedResource be supported by the Toolkit? By the eventual withResource official from
+      NgRx itself? I (Michael Small) am not sure and cannot speak for the NgRx team. But I am
+      experimenting.
+    </p>
   `,
 })
 export class App {}
