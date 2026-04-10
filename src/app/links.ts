9d1bb8d6404960e56e69cb2c20b97bf277f7825c
Michael Small <michaelsmalldev@gmail.com>
2026-04-09 21:39:10 -0500

    chore: link to prototypes in links component

--- src/app/links.ts
+++ src/app/links.ts
@@ -10,8 +10,18 @@ import { Component } from '@angular/core';
       </li>
       <li><a href="https://ngrx.io/guide/signals/signal-store" target="_blank">NgRx Signals</a></li>
       <li>
+        <a href="https://ngrx-toolkit.angulararchitects.io/docs/extensions" target="_blank"
+          >NgRx Toolkit</a
+        >
+      </li>
+      <li>
         <a href="https://github.com/ngrx/platform/issues/5121" target="_blank"
           >RFC(@ngrx/signals): Add delegatedSignal API</a
+        >
+      </li>
+      <li>
+        <a href="https://github.com/kobi2294/fun-with-projected-signals" target="_blank"
+          >Kobi Hari's projected signal prototype</a
         >
       </li>
       <li>
@@ -20,8 +30,8 @@ import { Component } from '@angular/core';
         >
       </li>
       <li>
-        <a href="https://ngrx-toolkit.angulararchitects.io/docs/extensions" target="_blank"
-          >NgRx Toolkit</a
+        <a href="https://github.com/markostanimirovic/extend-resource-prototype" target="_blank"
+          >Marko Stanimirovic's extend resource prototype</a
         >
       </li>
     </ul>
